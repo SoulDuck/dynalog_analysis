9fb382b0fa5528b6a7919d11a39a664c9288b902
kimseongjung <plznw4me@naver.com>
2017-10-12 00:45:40 +0900

    add save to format .mat

--- train_multi_lstm.py
+++ train_multi_lstm.py
@@ -37,6 +37,8 @@ min_ , max_ =data.get_min_max(train_xs, train_ys, test_xs, test_ys)
 print 'min', min_ , 'max' ,max_
 train_xs=train_xs/10000.
 test_xs=test_xs/10000.
+train_ys=train_ys/10000.
+
 #
 print train_xs.max()
 print train_xs.min()
